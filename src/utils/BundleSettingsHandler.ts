7300416249485aadf04496fdcb93aaa72d33c528
BitsAndDroids <info@BitsAndDroids>
2024-01-29 18:52:11 +0100

    removed unused import

--- src/utils/BundleSettingsHandler.ts
+++ src/utils/BundleSettingsHandler.ts
@@ -1,21 +1,20 @@
-import { Bundle } from '@/model/Bundle';
-import { info } from '@tauri-apps/plugin-log'
-import { Store } from 'tauri-plugin-store-api';
+import { Bundle } from "@/model/Bundle";
+import { Store } from "tauri-plugin-store-api";
 export class BundleSettingsHander {
   store: Store;
   bundleStore: Store;
 
   constructor() {
-    this.store = new Store('.settings.dat');
-    this.bundleStore = new Store('.bundleSettings.dat');
+    this.store = new Store(".settings.dat");
+    this.bundleStore = new Store(".bundleSettings.dat");
   }
 
   async getBundleSettings(): Promise<Bundle[]> {
-    const bundleKeys = await this.bundleStore.keys()
-    const bundles = []
+    const bundleKeys = await this.bundleStore.keys();
+    const bundles = [];
     for (const key of bundleKeys) {
-      const bundle = await this.bundleStore.get(key) as Bundle
-      bundles.push(bundle)
+      const bundle = (await this.bundleStore.get(key)) as Bundle;
+      bundles.push(bundle);
     }
     return bundles;
   }
