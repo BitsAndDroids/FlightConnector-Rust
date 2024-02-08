4a0b5acd5adf1068921e720123362e91438cdc50
bitsanddroids <info@bitsanddroids.com>
2024-02-08 15:01:59 +0100

    added store logic to the rust backend

--- src-tauri/src/events/bundle_registry.rs
+++ src-tauri/src/events/bundle_registry.rs
@@ -1,6 +1,5 @@
 use serde_json::json;
 use std::path::PathBuf;
-use tauri::api::path;
 use tauri_plugin_store::with_store;
 
 use tauri::Manager;
@@ -26,22 +25,25 @@ impl BundleRegistry {
     }
 
     pub fn load_bundle_settings(&mut self) {
-        if let Some(app_handle) = get_app_handle() {
-            let stores = app_handle.state::<StoreCollection<Wry>>();
-            let app_data_dir = app_handle
-                .app_handle()
-                .path_resolver()
-                .app_data_dir()
-                .unwrap();
-            let path = PathBuf::from(self.bundle_settings_path.clone());
-            let combined_path = app_data_dir.join(path);
-            println!("Loading bundle settings from {:?}", combined_path);
-            with_store(app_handle.app_handle(), stores, combined_path, |store| {
-                store.insert("test".to_string(), json!("b"))
-            })
-            .expect("failed to insert data");
-        } else {
-            println!("App handle not found");
-        }
+        //     if let Some(app_handle) = get_app_handle() {
+        //         let stores = app_handle.state::<StoreCollection<Wry>>();
+        //         let app_data_dir = app_handle
+        //             .app_handle()
+        //             .path_resolver()
+        //             .app_data_dir()
+        //             .unwrap();
+        //         let path = PathBuf::from(self.bundle_settings_path.clone());
+        //         let combined_path = app_data_dir.join(path);
+        //         println!("Loading bundle settings from {:?}", combined_path);
+        //         with_store(app_handle.app_handle(), stores, combined_path, |store| {
+        //             store
+        //                 .insert("test-from-rust".to_string(), json!("why is this not there"))
+        //                 .expect("failed to insert data");
+        //             store.save()
+        //         })
+        //         .expect("failed to process store");
+        //     } else {
+        //         println!("App handle not found");
+        //     }
     }
 }
