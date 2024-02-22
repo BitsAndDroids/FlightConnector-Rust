2f014759f4e5209c5cf541eb9aa0a522c7f2c732
BitsAndDroids <info@bitsanddroids.com>
2024-02-22 18:46:50 +0100

    Add option to stop connection

--- src/components/RunComponent.tsx
+++ src/components/RunComponent.tsx
@@ -2,7 +2,7 @@ import { ControllerSelectComponent } from "./ControllerSelectComponent";
 
 export const RunComponent = () => {
   return (
-    <div className={"flex flex-col"}>
+    <div className={"flex flex-col w-full justify-center items-center mt-16"}>
       <ControllerSelectComponent />
     </div>
   );
