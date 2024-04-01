67cca699fac8c322ca4dc98df15d23c29288741b
BitsAndDroids <info@bitsanddroids.com>
2024-04-01 14:25:13 +0200

    moved block down

--- src/components/RunComponent.tsx
+++ src/components/RunComponent.tsx
@@ -2,7 +2,7 @@ import { Dispatch, SetStateAction } from "react";
 import { ControllerSelectComponent } from "./ControllerSelectComponent";
 export const RunComponent = () => {
   return (
-    <div className={"flex flex-col w-full justify-center items-center"}>
+    <div className={"flex flex-col w-full justify-center items-center mt-12"}>
       <ControllerSelectComponent />
     </div>
   );
