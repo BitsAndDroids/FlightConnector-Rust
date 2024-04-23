db8871ed6bf8bc619f24c7c6002e0b9b54a1425b
BitsAndDroids <info@bitsanddroids.com>
2024-04-23 20:39:49 +0200

    removed print statements

--- crates/src-tauri/src/utils/wasm_installer.rs
+++ crates/src-tauri/src/utils/wasm_installer.rs
@@ -9,7 +9,6 @@ pub fn install_wasm(app: tauri::AppHandle, path: String) {
     println!("exe_path: {:?}", wasm_path);
     let mut files = return_files_in_dir(wasm_path.to_str().unwrap());
     for file in files {
-        println!("file_dir: {:?}", file);
         let file_path = file.path().clone();
         let relative_path = file_path.strip_prefix(&wasm_path);
         let dest_path = Path::new(&path)
@@ -18,8 +17,6 @@ pub fn install_wasm(app: tauri::AppHandle, path: String) {
         if let Some(parent_dir) = dest_path.parent() {
             std::fs::create_dir_all(parent_dir);
         }
-        //copy file to path
-        println!("Copying file to: {:?}", dest_path);
         std::fs::copy(file.path(), dest_path);
     }
 
