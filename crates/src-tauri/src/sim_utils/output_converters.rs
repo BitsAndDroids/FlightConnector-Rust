ad9341462cbe37844531330d17fd52df212d9fbd
BitsAndDroids <info@bitsanddroids.com>
2024-05-08 15:47:32 +0200

    option to reset default wasm events

--- crates/src-tauri/src/sim_utils/output_converters.rs
+++ crates/src-tauri/src/sim_utils/output_converters.rs
@@ -2,6 +2,10 @@ pub fn radian_to_degree(val: f64) -> i32 {
     (val * 180.0 / std::f64::consts::PI) as i32
 }
 
+pub fn val_to_dec(val: f64, places: u8) -> String {
+    format!("{:.1$}", val, places as usize)
+}
+
 pub fn value_to_inhg(val: f64) -> i32 {
     let multiplier = 0.01;
     // this is for 2 decimal places, for 3 decimal places, it would be 0.001, and so on
@@ -27,4 +31,8 @@ pub fn val_to_bool(val: f64) -> String {
 
 pub fn mps_to_kmh(val: f64) -> String {
     (val * 3.6).to_string()
+}
+
+pub fn mps_to_kts(val: f64) -> String {
+    (val * 1.94384).to_string()
 }
