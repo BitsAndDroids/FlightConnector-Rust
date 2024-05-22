a57ef6480ccb67bcfa32581d536fe2be79d2bfc8
BitsAndDroids <info@bitsanddroids.com>
2024-05-22 21:25:36 +0200

    added filter and sort functions to event editor

--- src/app/options/outputs/custom/CustomEvents.tsx
+++ src/app/options/outputs/custom/CustomEvents.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/elements/Button";
 import { Input } from "@/components/elements/Input";
+import { Select } from "@/components/elements/Select";
 import { Header } from "@/components/elements/header";
 import { EventEditor } from "@/components/outputs/EventEditor";
 import { WASMEventTable } from "@/components/wasm/WASMEventTable";
@@ -8,13 +9,39 @@ import { CustomEventHandler } from "@/utils/CustomEventHandler";
 import { invoke } from "@tauri-apps/api/core";
 import { useEffect, useState } from "react";
 
+interface SortSettings {
+  sortBy: keyof WASMEvent;
+  sortOrder: "desc" | "asc";
+  outputFormat: string | undefined;
+  type: string | undefined;
+}
 export const CustomEvents = () => {
+  const [sortSettings, setSortSettings] = useState<SortSettings>({
+    sortBy: "id",
+    sortOrder: "asc",
+    outputFormat: undefined,
+    type: undefined,
+  });
   const [events, setEvents] = useState<WASMEvent[]>([]);
   const [filteredEvents, setFilteredEvents] = useState<WASMEvent[]>([]);
   const [eventEditorOpen, setEventEditorOpen] = useState<boolean>(false);
   const [eventToEdit, setEventToEdit] = useState<WASMEvent | undefined>(
     undefined,
   );
+  const keyArray: Array<keyof WASMEvent> = [
+    "id",
+    "action",
+    "action_type",
+    "action_text",
+    "output_format",
+    "update_every",
+    "min",
+    "max",
+    "value",
+    "offset",
+    "plane_or_category",
+  ];
+
   const wasmStore = new CustomEventHandler();
   useEffect(() => {
     const fetchWasmEvents = async () => {
@@ -28,9 +55,44 @@ export const CustomEvents = () => {
     fetchWasmEvents();
   }, []);
 
+  const sortEvents = () => {
+    const sortedEvents = events.sort((a, b) => {
+      const aValue = a[sortSettings.sortBy];
+      const bValue = b[sortSettings.sortBy];
+      if (typeof aValue === "number" && typeof bValue === "number") {
+        return sortSettings.sortOrder === "asc"
+          ? aValue - bValue
+          : bValue - aValue;
+      }
+      if (typeof aValue === "string" && typeof bValue === "string") {
+        console.log("string sort");
+        return sortSettings.sortOrder === "asc"
+          ? aValue.localeCompare(bValue)
+          : bValue.localeCompare(aValue);
+      } else {
+        return 0;
+      }
+    });
+    setFilteredEvents(sortedEvents);
+  };
+
   const addEvent = async () => {
     console.log("add event");
     setEventEditorOpen(true);
+  };
+
+  const changeSortBy = (sortBy: string) => {
+    const key = sortBy as keyof WASMEvent;
+    setSortSettings({ ...sortSettings, sortBy: key });
+    sortEvents();
+  };
+
+  const changeSortOrder = (sortOrder: string) => {
+    if (sortOrder !== "asc" && sortOrder !== "desc") {
+      return;
+    }
+    setSortSettings({ ...sortSettings, sortOrder: sortOrder });
+    sortEvents();
   };
 
   const saveEvent = async (event: WASMEvent) => {
@@ -82,7 +144,9 @@ export const CustomEvents = () => {
   };
 
   const searchId = async (id: string) => {
-    const filteredEvents = events.filter((event) => event.id.toString() === id);
+    const filteredEvents = events.filter((event) =>
+      event.id.toString().includes(id),
+    );
     setFilteredEvents(filteredEvents);
   };
 
@@ -114,6 +178,24 @@ export const CustomEvents = () => {
             placeholder="Search by id"
             addToClassName="mt-10"
             onChange={(id: string) => searchId(id)}
+          />
+          <Select
+            label="Sort By"
+            labelPosition="top"
+            options={keyArray}
+            values={keyArray}
+            value={sortSettings.sortBy}
+            onChange={changeSortBy}
+            addToClassName="mt-2 text-white"
+          />
+          <Select
+            label="Sort Order"
+            labelPosition="top"
+            options={["asc", "desc"]}
+            values={["asc", "desc"]}
+            value={sortSettings.sortOrder}
+            onChange={changeSortOrder}
+            addToClassName="mt-2 text-white"
           />
         </div>
         <WASMEventTable
