9afaa532fa094abec2f591b5092640cac5443cb0
BitsAndDroids <info@bitsanddroids.com>
2024-06-05 21:45:50 +0200

    added min throttle value to mapping

--- crates/src-tauri/src/sim_utils/input_converters.rs
+++ crates/src-tauri/src/sim_utils/input_converters.rs
@@ -12,12 +12,13 @@ pub fn convert_dec_to_dcb(val: u32) -> u32 {
     result
 }
 
-pub fn map_analog_to_axis(val: i32) -> i32 {
-    let analog_min = 0;
-    let analog_max = 1023;
+pub fn map_analog_to_axis(min: f32, val: i32) -> i32 {
+    let analog_min = 0.0;
+    let analog_max = 1023.0;
 
-    let axis_min = -16383;
-    let axis_max = 16383;
+    let axis_min = -16383.0 - (-16383.0 * min);
+    let axis_max = 16383.0;
+    let conv_val = val as f32;
 
-    axis_min + (axis_max - axis_min) * (val - analog_min) / (analog_max - analog_min)
+    (axis_min + (axis_max - axis_min) * (conv_val - analog_min) / (analog_max - analog_min)) as i32
 }
