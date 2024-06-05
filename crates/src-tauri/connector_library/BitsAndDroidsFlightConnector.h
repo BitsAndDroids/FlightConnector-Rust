5645b47a94291dae368f8b0627388ac39dc1f570
BitsAndDroids <info@bitsanddroids.com>
2024-06-05 20:50:35 +0200

    upped mixture and prop count to 4

--- crates/src-tauri/connector_library/BitsAndDroidsFlightConnector.h
+++ crates/src-tauri/connector_library/BitsAndDroidsFlightConnector.h
@@ -656,8 +656,10 @@ public:
   void superAdvancedInputHandling(byte eng1Percentage, byte eng2Percentage,
                                   byte eng3Percentage, byte eng4Percentage);
   void sendCombinedMixtureValues();
-  void propsInputHandling(int propPin1, int propPin2);
-  void mixtureInputHandling(int mixturePin1, int mixturePin2);
+  void propsInputHandling(int propPin1, int propPin2, int propPin3,
+                          int propPin4);
+  void mixtureInputHandling(int mixturePin1, int mixturePin2, int mixturePin3,
+                            int mixturePin4);
   void sendSetElevatorTrim(int value);
   void sendFlaps();
   void setPotFlaps(byte flapsPin);
@@ -1012,22 +1014,26 @@ private:
   int prop2 = 0;
 
   int engines[4] = {0, 0, 0, 0};
-  int mixturePercentage[2] = {0, 0};
-  int props[2] = {0, 0};
+  int mixturePercentage[4] = {0, 0, 0, 0};
+  int props[4] = {0, 0, 0, 0};
 
   int flaps;
   int oldFlaps;
 
   int value;
-  int propValue1;
-  int propValue2;
   int oldValue;
 
   int navObs1;
   int navObs2;
 
+  int propValue1;
+  int propValue2;
+  int propValue3;
+  int propValue4;
   int oldPropValue1;
   int oldPropValue2;
+  int oldPropValue3;
+  int oldPropValue4;
 
   int valueEng1;
   int valueEng2;
@@ -1041,8 +1047,12 @@ private:
 
   int mixtureValue1;
   int mixtureValue2;
+  int mixtureValue3;
+  int mixtureValue4;
   int oldMixtureValue1;
   int oldMixtureValue2;
+  int oldMixtureValue3;
+  int oldMixtureValue4;
 
   int oldTrim;
   int currentTrim;
