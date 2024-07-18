4258e8c2e33e8bc8e6e46c248b3bca1480f2c92f
BitsAndDroids <info@bitsanddroids.com>
2024-07-18 12:10:40 +0200

    added optional checks to the settings initialization

--- crates/types/src/types/connector_settings.rs
+++ crates/types/src/types/connector_settings.rs
@@ -1,8 +1,15 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct SavedConnectorSettings {
+    pub use_trs: Option<bool>,
+    pub adc_resolution: Option<i32>,
+    pub installed_wasm_version: Option<String>,
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct ConnectorSettings {
     pub use_trs: bool,
     pub adc_resolution: i32,
-    pub installed_wasm_version: Option<String>,
+    pub installed_wasm_version: String,
 }
