ae9d39b9dba3ee6a6c2182d69208570c82e96ffb
BitsAndDroids <info@bitsanddroids.com>
2024-08-23 11:31:11 +0200

    fixed default settings

--- crates/types/src/types/connector_settings.rs
+++ crates/types/src/types/connector_settings.rs
@@ -12,4 +12,16 @@ pub struct ConnectorSettings {
     pub use_trs: bool,
     pub adc_resolution: i32,
     pub installed_wasm_version: String,
+    pub send_every_ms: u64,
+}
+
+impl From<SavedConnectorSettings> for ConnectorSettings {
+    fn from(saved: SavedConnectorSettings) -> Self {
+        ConnectorSettings {
+            use_trs: saved.use_trs.unwrap_or(false),
+            adc_resolution: saved.adc_resolution.unwrap_or(1023),
+            installed_wasm_version: saved.installed_wasm_version.unwrap_or("".to_string()),
+            send_every_ms: saved.send_every_ms.unwrap_or(6),
+        }
+    }
 }
