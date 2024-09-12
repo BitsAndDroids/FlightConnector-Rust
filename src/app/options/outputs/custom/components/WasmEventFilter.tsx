13dc09a564ce941ae1461383bdf49986a9f9d053
BitsAndDroids <info@bitsanddroids.com>
2024-09-12 13:51:14 +0200

    added filtercard ui element

---
+++ src/app/options/outputs/custom/components/WasmEventFilter.tsx
@@ -0,0 +1,24 @@
+import { FilterCard } from "@/components/card/FilterCard";
+import { Input } from "@/components/elements/inputs/Input";
+import { Select } from "@/components/elements/Select";
+import { ReactNode, useState } from "react";
+
+export const WasmEventFilter = () => {
+  const [filter, setFilter] = useState({
+    categories: "All",
+    type: "All",
+  });
+  const filterContent: ReactNode = (
+    <div>
+      <Input label="Filter" onLight={false} />
+      <Select
+        label="Type"
+        onLight={false}
+        options={["All", "Input", "Output"]}
+        values={["All", "Input", "Output"]}
+        value={filter.type}
+      />
+    </div>
+  );
+  return <FilterCard children={filterContent} />;
+};
---
+++ src/components/card/FilterCard.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export const FilterCard = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="mr-4 shadow-lg box-shadow p-4 bg-gradient-to-b from-[#305d7b] to-[#596d96] bg-opacity-20 rounded-md">
+      {children}
+    </div>
+  );
+};
