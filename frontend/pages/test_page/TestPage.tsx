865bc4bf3bdf17e657c24c584b4d5da6bbb0ce82
BitsAndDroids <info@bitsanddroids.com>
2024-11-10 19:35:43 +0100

    added event tester tests

--- frontend/pages/test_page/TestPage.tsx
+++ frontend/pages/test_page/TestPage.tsx
@@ -14,13 +14,19 @@ export const TestPage = () => {
     <div className="bg-bitsanddroids-blue h-screen min-w-screen min-h-screen mt-10 flex flex-col items-center justify-start max-w-[400px]">
       <div className="w-3/4">
         <Header title="Test Page" level={1} />
-        <Input label="ID" onChange={(val) => setId(parseInt(val as string))} />
         <Input
+          testid="input_id"
+          label="ID"
+          onChange={(val) => setId(parseInt(val as string))}
+        />
+        <Input
+          testid="input_value"
           label="Value"
           type="number"
           onChange={(val) => setValue(parseInt(val as string))}
         />
         <Button
+          testid="btn_send"
           text="Send"
           addToClassName="mt-4"
           onClick={() => {
